ca69a9bcd40c409054b97efed860cf1e76fc3382
arekbee <arekbee@gmail.com>
2013-10-26 10:43:19 +0200

    Adding standard code snippets

---
+++ CodeSnippets/MyCodeSnippets/MyCodeSnippets/Deployment/Code/MyCodeSnippetsDeployment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCodeSnippets.Deployment.Code
+{
+    public class MyCodeSnippetsDeployment
+    {
+        static int Main(string[] args)
+        {
+            var snippetFiles = Directory.GetFiles("Snippets","*.snippet");
+            string destPathMyDoc= Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string destPath = Path.Combine(destPathMyDoc, @"Visual Studio 2012\Code Snippets\Visual C#\My Code Snippets");
+            foreach (var file in snippetFiles)
+            {
+                try
+                {
+                    string dest = Path.Combine(destPath, Path.GetFileName(file));
+                    File.Copy(file, dest);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error:"+ex.Message);
+                }
+            }
+            Console.WriteLine("Done");
+            Console.ReadKey();
+            return 0;
+        }
+
+    }
+}
---
+++ CodeSnippets/MyCodeSnippets/MyCodeSnippets/bin/Debug/Deployment/BatchFile/deploy.bat
@@ -0,0 +1,1 @@
+xcopy   ..\..\Snippets\*.snippet "%USERPROFILE%\Documents\Visual Studio 2012\Code Snippets\Visual C#\My Code Snippets\" /k
